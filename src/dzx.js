6f0fc24a2474c6c01d1a16fa6b92242e5a35dfce
FooTearth <footearth@gmail.com>
2021-06-03 02:18:02 +0800

    202106003_1

--- src/dzx.js
+++ src/dzx.js
@@ -1,11 +1,32 @@
 #!/usr/bin/env dzx
-/// <reference path="https://deno.land/x/dzx@0.2.3/types.d.ts" />
-
 import Denomander from "https://deno.land/x/denomander/mod.ts"
 
 $.verbose = true
 $.shell = "/usr/local/env bash"
 
-import { $, cd, fs, io, log, path } from "https://deno.land/x/dzx@0.2.3/mod.ts"
+// import { $, cd, fs, io, log, path } from "https://deno.land/x/dzx@0.2.3/mod.ts"
 
 console.log(`Hello from ${$.blue.bold("dzx")}!`)
+
+const program = new Denomander({
+  app_name: 'Mooxe Dockers builder'
+, app_description: 'build mooxe-docker images'
+, app_version: '0.0.0'
+})
+
+// console.log(Deno.args.slice(1))
+
+program
+// globalOption
+.baseOption('-n --name', 'build docker name')
+
+.command('build', 'build docker images')
+.option('-l --layers', 'build with layers mode')
+.option('-nc --no-cache', 'build with no-cache mode')
+
+.parse(Deno.args.slice(1))
+
+// if (program.address) {
+//   const port = program.port || "8000"
+//   console.log(`Server is running on ${program.address}:${port}`)
+// }
--- src/index.js
+++ src/index.js
@@ -1,16 +1,6 @@
-// import Denomander from "https://deno.land/x/denomander/mod.ts";
-import {
-  $, cd, fs, io, log, path
-} from "https://deno.land/x/dzx@0.2.3/mod.ts";
+import basePrd from './ubuntu/base/prd.js'
+import baseDev from './ubuntu/base/dev.js'
+import node from './ubuntu/node.js'
+import deno from './ubuntu/deno.js'
 
-$.verbose = true
-$.shell = "/usr/bin/bash"
-
-console.log(`Hello from ${$.blue.bold("dzx")}!`);
-
-// import baseDev from './ubuntu/base/dev.js'
-// import basePrd from './ubuntu/base/prd.js'
-// import node from './ubuntu/node.js'
-// import deno from './ubuntu/deno.js'
-
-// console.log(deno())
+console.log(deno())
