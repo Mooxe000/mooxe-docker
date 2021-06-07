5869d844b9625dfe1f8a8003edd779bffa7218bc
FooTearth <footearth@gmail.com>
2021-06-07 12:41:48 +0800

    move main command codes from dzx.js to src/index.js

--- dzx.js
+++ dzx.js
@@ -1,36 +1,11 @@
 #!/usr/bin/env -S deno run -A --unstable --import-map=import_map.json https://deno.land/x/dzx@0.2.3/dzx.ts
 /// <reference path="https://deno.land/x/dzx@0.2.3/types.d.ts" />
 
-import Denomander from 'denomander'
-import DockerFileObj from './src/ubuntu/index.js'
-const DockerFiles = DockerFileObj()
-
-import cmdBuild from './src/commands/build.js'
+import run from './src/index.js'
 
 $.verbose = true
 $.shell = "/usr/bin/bash"
 
 console.log(`Hello from ${$.blue.bold("dzx")}!`)
 
-const program = new Denomander({
-  app_name: 'Mooxe Dockers builder'
-, app_description: 'build mooxe-docker images'
-, app_version: '0.0.0'
-})
-
-program
-// globalOption
-// baseOption
-.globalOption('-n --name', 'build docker name')
-
-.command('build', 'build docker images')
-.option('-m --mode', `
-
-  build with layers/no-cache mode.
-  default mode: no-cache
-`)
-.action( cmdBuild(program, DockerFiles) )
-
-.command('in', 'runin docker images')
-
-.parse(Deno.args.slice(1))
+run()
--- src/index.js
+++ src/index.js
@@ -1,6 +1,32 @@
-import basePrd from './ubuntu/basePrd.js'
-import baseDev from './ubuntu/baseDev.js'
-import node from './ubuntu/node.js'
-import deno from './ubuntu/deno.js'
+import Denomander from 'denomander'
+import DockerFileObj from './ubuntu/index.js'
+const DockerFiles = DockerFileObj()
 
-console.log(deno())
+import cmdBuild from './commands/build.js'
+
+export default () => {
+
+  const program = new Denomander({
+    app_name: 'Mooxe Dockers builder'
+  , app_description: 'build mooxe-docker images'
+  , app_version: '0.0.0'
+  })
+
+  program
+  // globalOption
+  // baseOption
+  .globalOption('-n --name', 'build docker name')
+
+  .command('build', 'build docker images')
+  .option('-m --mode', `
+
+    build with layers/no-cache mode.
+    default mode: no-cache
+  `)
+  .action( cmdBuild(program, DockerFiles) )
+
+  .command('in', 'runin docker images')
+
+  .parse(Deno.args.slice(1))
+
+}
