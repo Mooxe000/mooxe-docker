3b5abbf46e1b67c05a1c7033407b175a21c7236f
FooTearth <footearth@gmail.com>
2021-07-26 13:57:34 +0800

    completed use dvm to manage deno's version

--- src/ubuntu/deno.js
+++ src/ubuntu/deno.js
@@ -1,33 +1,57 @@
 import DockerFile from '../Docker_file.js'
-import snippets from './snippets/index.js'
+// import snippets from './snippets/index.js'
+
+const version = '1.12.1'
+
+const dvmRun = cmd => `
+  bash -lc "${cmd}"
+`
 
 const dockerfile = () =>
 
   DockerFile()
   .from`mooxe/node`
 
+  // .run(`
+  //   ${snippets.install()`unzip`} &&
+  //   curl -fsSL https://deno.land/x/install/install.sh | sh -s v1.10.3
+  // `)
+
   .run(`
-    ${snippets.install()`unzip`} &&
-    curl -fsSL https://deno.land/x/install/install.sh | sh -s v1.10.3
+    curl -fsSL https://deno.land/x/dvm/install.sh | sh
   `)
 
-  .run`
+  .run(`
     echo "
-      export DENO_INSTALL=\\"\\$HOME/.deno\\"\\n
-      export PATH=\\"\\$DENO_INSTALL/bin:\\$PATH\\"\\n
+      source $HOME/.bash_profile
     " >> $HOME/.bashrc &&
     echo "
-      export DENO_INSTALL=\\"\\$HOME/.deno\\"\\n
-      export PATH=\\"\\$DENO_INSTALL/bin:\\$PATH\\"\\n
+      source $HOME/.bash_profile
+    " >> $HOME/.zshrc &&
+    echo "
+      source $HOME/.bash_profile
     " >> $HOME/.config/fish/config.fish
-  `
+  `)
 
-  // TODO dvm
+  .run(dvmRun(`
+    dvm install ${version}
+  `))
+
+  // .run`
+  //   echo "
+  //     export DENO_INSTALL=\\"\\$HOME/.deno\\"\\n
+  //     export PATH=\\"\\$DENO_INSTALL/bin:\\$PATH\\"\\n
+  //   " >> $HOME/.bashrc &&
+  //   echo "
+  //     export DENO_INSTALL=\\"\\$HOME/.deno\\"\\n
+  //     export PATH=\\"\\$DENO_INSTALL/bin:\\$PATH\\"\\n
+  //   " >> $HOME/.config/fish/config.fish
+  // `
 
   .run(
     [
       // velociraptor
-      'deno install -qAn vr https://deno.land/x/velociraptor@1.0.0/cli.ts'
+      'deno install -qAn vr https://deno.land/x/velociraptor@1.0.1/cli.ts'
       // dzx 0.2.3
     ,
       'deno install --allow-all -r -f --unstable https://deno.land/x/dzx@0.2.3/dzx.ts'
@@ -38,6 +62,7 @@ const dockerfile = () =>
     .map( c => `/bin/fish -lc "${c}"` )
     .join(" && ")
   )
+
   ()
 
 export default dockerfile
