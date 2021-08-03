a9c8b313b309f4722d9d25c79a2632a03b2ccb4f
FooTearth <footearth@gmail.com>
2021-08-03 16:34:33 +0800

    rebuild nim

--- src/ubuntu/nim.js
+++ src/ubuntu/nim.js
@@ -1,16 +1,43 @@
 import DockerFile from '../Docker_file.js'
 import snippets from './snippets/index.js'
 
+const env = [
+  'export NIM_HOME=\\$HOME/.nim'
+, 'export PATH=\\$NIM_HOME/bin:\\$PATH'
+]
+
+const deps = [
+  'gcc'
+, 'make'
+]
+
 const dockerfile = () =>
 
   DockerFile()
   .from`mooxe/rescript`
 
-  //   curl https://nim-lang.org/choosenim/init.sh -sSf | sh
+  // .run(
+  //   snippets
+  //   .install()`nim`
+  // )
+
   .run(
     snippets
-    .install()`nim`
+    .install()(deps)
   )
+
+  .run(`
+    git clone --depth 1 https://github.com/nim-lang/Nim.git &&
+    mv Nim .nim &&
+    cd .nim &&
+    ./build_all.sh
+  `)
+
+  .run(`
+    echo "${env.join('\\n')}" >> $HOME/.bash_profile &&
+    echo "${env.join('\\n')}" >> $HOME/.zshrc &&
+    echo "${env.join('\\n')}" >> $HOME/.config/fish/config.fish
+  `)
 
   ()
 
