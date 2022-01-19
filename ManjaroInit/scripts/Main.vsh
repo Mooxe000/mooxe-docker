22a30bb4c650ade7ba8a6c1ff883cf40ca1dda0e
footearth <footearth@gmail.com>
2022-01-19 14:54:15 +0800

    remove lang test

--- ManjaroInit/scripts/Main.vsh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env -S v run
-
-for _ in 0 .. 5 {
-  println('V script')
-}
--- examples/deno/src/Utils/index.js
+++
@@ -1,9 +0,0 @@
-import Instance from './Instance.js'
-import Infixr from './Infixr.js'
-import { curry } from './Curry.js'
-
-export {
-  Instance
-, Infixr
-, curry
-}
